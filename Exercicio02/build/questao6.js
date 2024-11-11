554ff082248991a04f9c93f3e44274fd4b653c27
samleticias <sammyaleticia09@gmail.com>
2024-11-11 15:43:04 -0300

    atividade 02

---
+++ Exercicio02/build/questao6.js
@@ -0,0 +1,14 @@
+var Professor = /** @class */ (function () {
+    function Professor() {
+        this.name = "Ely";
+        this.payment = 120.56;
+        this.preferredLanguage = "TypeScript";
+    }
+    return Professor;
+}());
+function main() {
+    var professor = new Professor;
+    console.log("".concat(professor.name, "\nMy payment time is ").concat(professor.payment, "\nand\nmy preffered language is ").concat(professor.preferredLanguage));
+}
+main();
+//# sourceMappingURL=questao6.js.map
---
+++ Exercicio02/build/questao7.js
@@ -0,0 +1,4 @@
+// Erro com strictNullChecks: true
+var nome = null; // Erro: Type 'null' is not assignable to type 'string'
+var nome = null; // Corrige erro declarando que a variável pode aceitar null
+//# sourceMappingURL=questao7.js.map
